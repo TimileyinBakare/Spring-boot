e2158b21fbcf22aea846c08888241ec8d929e398
TimileyinBakare <bakaretim18@gmail.com>
2023-10-18 16:06:52 +0100

    New branch for assignment and work

--- src/main/java/com/SpringExamples/Tutorial/TutorialApplication.java
+++ src/main/java/com/SpringExamples/Tutorial/TutorialApplication.java
@@ -8,6 +8,8 @@ public class TutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialApplication.class, args);
+		//This is for adding assignment and tips cause i know you might be busy
+		// you can just drop anything on here you want me to do
 	}
 
 }
